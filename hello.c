e8dde35436ad6a76d60003ee87f83449712b6623
pwebb2002 <pwebb4@huskers.unl.edu>
2020-08-24 01:58:52 +0000

    added my info

--- hello.c
+++ hello.c
@@ -11,6 +11,8 @@
 int main(int argc, char **argv){
     printf("Hello David Dubchakov\n");
     printf("I am majoring in Computer Science/Pre-Med\n");
+    printf("Payton Webb");
+    printf("I am majoring in Computer Science");
 
 
 
